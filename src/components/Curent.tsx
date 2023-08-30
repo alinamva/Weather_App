ea11a6e859823203f35adacb9fb02f81dd1e2371
unknown <alinamva>
2023-08-30 23:28:35 +0400

    component change

--- src/components/Curent.tsx
+++ src/components/Curent.tsx
@@ -1,32 +1,37 @@
-import { MapIcon } from "@heroicons/react/24/outline";
 import cloudrain from "../assets/cloudrain.png";
-const Curent = () => {
+
+import WaterIcon from "@mui/icons-material/Water";
+import WaterDropIcon from "@mui/icons-material/WaterDrop";
+import AirIcon from "@mui/icons-material/Air";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+
+const Curent = (data: any) => {
   return (
-    <div className="bg-darkblue2/40 backdrop-blur-md rounded-xl p-4 text-white">
+    <div className="bg-darkblue2/40 backdrop-blur-md rounded-xl p-4 flex flex-col gap-4 text-white">
       <div>
         <h2>Current Weather</h2>
-        <span>6 PM</span>
+        <span>{data?.main?.pressure}</span>
       </div>
-      <div className="flex">
+      <div className="flex justify-between">
         <img src={cloudrain} className="w-16" />
-        <div>
-          <h2>24 C</h2>
+        <div className="flex flex-col">
+          <span className="text-3xl">24 C</span>
           <span>Heavy Rain</span>
         </div>
       </div>
       <div>
-        <ul className="flex gap-4 w-full">
+        <ul className="flex justify-between w-full">
           <li>
-            <MapIcon className="w-4" />
+            <WaterIcon className="w-4" />
           </li>
           <li>
-            <MapIcon className="w-4" />
+            <WaterDropIcon className="w-4" />
           </li>
           <li>
-            <MapIcon className="w-4" />
+            <AirIcon className="w-4" />
           </li>
           <li>
-            <MapIcon className="w-4" />
+            <WbSunnyIcon className="w-4" />
           </li>
         </ul>
       </div>
--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,20 +1,50 @@
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 import Curent from "../components/Curent";
+import Search from "./Search";
+import useSWR from "swr";
+import axios from "axios";
+
+export interface IWindow {
+  city: string;
+  setCity: (city: string) => void;
+  handleButtonClick: () => void;
+}
+
 const Window = () => {
+  const [city, setCity] = useState("");
+
+  const handleButtonClick = () => {
+    setCity("hello");
+  };
+  const api_key = "d5dc87c5ff563395e08edb6318fb7dea";
+  const fetcher = async (url: string) => {
+    const response = await axios.get(url);
+    return response.data;
+  };
+
+  const apiURl = (cityName: string) =>
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=Metric&appid=${api_key}`;
+
+  const { data, error, isLoading } = useSWR(
+    city ? apiURl(city) : null,
+    fetcher
+  );
+  console.log("City:", city);
+  console.log("Data:", data);
+  console.log("Error:", error);
+  console.log("IsValidating:", isLoading);
+
+  if (error) return <div>Error</div>;
+  if (isLoading) return <div>Loading ... </div>;
   return (
     <div className="p-10 flex flex-col gap-5">
-      <div className="flex text-white">
-        <div className="bg-darkblue2/40 rounded-l-xl backdrop-blur-md py-2 px-1  ">
-          <MagnifyingGlassIcon className="w-5" />
-        </div>
-        <input
-          className="bg-darkblue2/40 rounded-r-xl backdrop-blur-md outline-none"
-          placeholder="Search for location"
-        />
-      </div>
-      <div>
-        <Curent />
-      </div>
+      <h1>hello {data?.name}</h1>
+      <Search
+        setCity={setCity}
+        handleButtonClick={handleButtonClick}
+        city={city}
+      />
+      <Curent data={data} city={city} />
     </div>
   );
 };
